e27bd4da218c17da40318f6b6180d4fc8c095b09
Michael Baumann <michi.don@gmail.com>
2015-03-08 15:03:24 +0100

    Rewrite of main()
    Now with new features implemented.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
-#include <QCoreApplication>
+#include <QtCore/QCoreApplication>
+
 #include "Network.IRC.Server.hpp"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    Network::IRC::Server srv("irc.rizon.net", 9999, &a);
-    srv.connect("testos", "Test-App", true);
+    Network::IRC::Server *srv = new Network::IRC::Server("irc.rizon.net", 9999);
+    // Join channel when connected to server
+    // Now with 100% more lambda voodoo
+    QObject::connect(srv, &Network::IRC::Server::connected, [&](){ srv->join("mr-bigbang"); });
+    QObject::connect(srv, &Network::IRC::Server::quitting, &a, &QCoreApplication::quit);
+
+    srv->connect("testos", "Test-App", true);
 
     return a.exec();
 }
